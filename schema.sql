e8b3299042f482e6c440ea61cb88ce25eb578549
Brandon O <b.ondayko@gmail.com>
2021-01-05 16:56:23 -0600

    several tables added

--- schema.sql
+++ schema.sql
@@ -1,5 +1,8 @@
 USE employee;
 DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS managers;
 
 CREATE TABLE employees (
     id int (20) AUTO_INCREMENT,
@@ -7,5 +10,21 @@ CREATE TABLE employees (
     first_name VARCHAR(30) NOT NULL,
     last_name  VARCHAR(20) NOT NULL,
     role_id INT,
-    manager_id INT
+    manager_id INT,
+);
+
+CREATE TABLE roles (
+    title VARCHAR(30),
+    salary decimal,
+    department_id INT,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES employees(role_id)
+);
+
+CREATE TABLE departments (
+    name VARCHAR(30),
+);
+
+CREATE TABLE managers (
+    name VARCHAR(30),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(manager_id)
 );
